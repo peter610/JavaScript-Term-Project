36991df0061a6e04acac0d788e2508c9d5405550
peter610 <speterson88@att.net>
2018-10-23 16:55:56 -0400

    milestone1

---
+++ FinalProject/js/FinalProject.js
@@ -0,0 +1,9 @@
+function openPage(evt, pageName){
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for(i=0; i <tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+    document.getElementById(pageName).style.display = "block";
+}
